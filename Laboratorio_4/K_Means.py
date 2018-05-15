3f7038da63176e7868aa6feed1ed8d0b8808fd68
FedericoFioritto <Fede66513@hotmail.com>
2018-05-15 00:43:06 -0300

    k means validacion

--- Laboratorio_4/K_Means.py
+++ Laboratorio_4/K_Means.py
@@ -1,8 +1,12 @@
 import random
 import math
-class KMeans:
+from functools import reduce
+import sys
+
+
+class K_Means:
 	def __init__(self, k, max_iter, times):
-		# k: Número de clusters. 
+		# k: Número de clusters.
 		# max_iter: Máximo de iteraciones (si no se converge).
 		# times: Cantidad de veces que se corre el algoritmo con distintas inicializaciones.
 		self.k = k
@@ -11,42 +15,89 @@ class KMeans:
 
 		self.optimal = None
 
-	
 	def train(self, data):
-
 		intermediate_results = []
 
+		file = open('results.txt', 'w')
+
+		file.write('-----------------------------------------------------')
+		file.write('\n')
+		file.write('ALGORITMO K-Means ') 
+		file.write('\n')
+		file.write('\n')
+		file.write('times = ') 
+		file.write(str(self.times))
+		file.write('\n')
+		file.write('k = ')
+		file.write(str(self.k))
+		file.write('\n')
+		file.write('max_iter = ')
+		file.write(str(self.max_iter))
+		file.write('\n')
+
 		for i in range(self.times):
+			file.write('-----------------------------------------------------')
+			file.write('\n')
+			file.write('Iteración ')
+			file.write(str(i))
+			file.write('\n')
+			file.write('\n')
 
 			# Selecciono k centroides iniciales al azar para la corrida i del algoritmo.
-			centroids = random.sample(data, self.k)
+			centroids = random.sample(list(data), self.k)
+
+			file.write('Selecciona centroides al azar')
+			file.write('\n')
+			for centroid in centroids:
+				file.write(str(centroid))
+				file.write('\n')
 
 			# Asigno los centroides aleatoriamente al conjunto de entrenamiento
-			assigned_centroids = [random.choice(range(k)) for iter in range(k)]
+			assigned_centroids = [random.choice(range(self.k)) for iter in range(len(data))]
 
 			j = 1
 			past_assigned_centroids = []
-			while past_assigned_centroids != assigned_centroids and j <= max_iter: 	
+			while past_assigned_centroids != assigned_centroids and j <= self.max_iter: 	
 				past_assigned_centroids = assigned_centroids
 				j += 1
 
 				# Recorro conjunto de entrenamiento.
-				for index, instance in data:
+				for index, instance in enumerate(data):
 
 					# Asigno centroides a los ejemplos.
 					assigned_centroids[index] = self.assign(instance, centroids)
 				
 				# Recorro los centroides.
-				for index_to_update, centroid_to_update in centroids:
+				for index_to_update, centroid_to_update in enumerate(centroids):
 
 					# Actualizo lista de centroides. 
 					centroids[index_to_update] = self.update(index_to_update, centroid_to_update, assigned_centroids, data)
+
 
 			# Almaceno los centroides hallados junto con sus asignaciones en cada corrida del algoritmo.
 			intermediate_results.append((centroids, assigned_centroids))
 
 		# Nos quedamos con el resultado óptimo medido a través de la distancia euclidiana.
 		self.optimal = self.get_best_result(intermediate_results, data)
+
+		file.write('\n')
+		file.write('\n')
+		file.write('\n')
+		file.write('Los centroides hallados son: ')
+		file.write('\n')
+		file.write('\n')
+		for centroid in self.optimal[0]:
+			for coord in centroid:
+				file.write(str(round(coord, 3)))
+				file.write(' , ')
+			file.write('\n')
+			file.write('\n')
+			file.write('*******************************')
+			file.write('\n')
+			file.write('\n')
+		
+
+		file.close()
 
 	def assign(self, instance, centroids):
 		# Devuelve el índice del centroide cuya distancia es menor al ejemplo dado.
@@ -67,9 +118,9 @@ class KMeans:
 		# de la lista de centroides.
 
 		numerator = centroid_to_update
-
+		denominator = 1
 		# Recorro lista de centroides asignados.
-		for index, assigned_centroid in assigned_centroids:
+		for index, assigned_centroid in enumerate(assigned_centroids):
 
 			# Si un ejemplo tiene asignado el centroide a actualizar.
 			if index_to_update == assigned_centroid:
@@ -77,32 +128,30 @@ class KMeans:
 				# Sumo el ejemplo que posee el centroide a actualizar.
 				numerator = self.vector_sum(data[index], numerator)
 				denominator += 1
+		return list(map(lambda elem: elem/denominator, numerator))
 
-		return map(lambda elem: elem/denominator, numerator)
+	def euclidean_distance(self, X, Y):
+		return math.sqrt(reduce(lambda acc, elem: acc+elem, [(x - y)**2 for x, y in zip(X, Y)]))
 
-	def euclidean_distance(X, Y):
-		return math.sqrt([(x - y)**2 for x, y in zip(X, Y)])
-
-	def vector_sum(X, Y):
+	def vector_sum(self, X, Y):
 		# Suma los vectores X e Y.
 		return [x + y for x, y in zip(X, Y)]
 
 	def get_best_result(self, intermediate_results, data):
 		best_result_index = 0
-		centroids = intermediate_results[0][0] 
+		centroids = intermediate_results[0][0]
 		assigned_centroids = intermediate_results[0][1] 
 		
 		best_result = 0
-		for index, instance in data:
+		for index, instance in enumerate(data):
 			best_result += self.euclidean_distance(instance, centroids[assigned_centroids[index]])
-
 
 		for i in range(1, len(intermediate_results)):
 			centroids = intermediate_results[i][0] 
 			assigned_centroids = intermediate_results[i][1] 
 
 			result = 0
-			for index, instance in data:
+			for index, instance in enumerate(data):
 				result += self.euclidean_distance(instance, centroids[assigned_centroids[index]])
 
 			if result < best_result:
--- Laboratorio_4/main.py
+++ Laboratorio_4/main.py
@@ -3,11 +3,18 @@ import utils
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from anomaliesDetection import AnomaliesDetection
 from naive_bayes import NaiveBayes
+from K_Means import K_Means
+from sklearn.cluster import KMeans
 
 
 #####################################################################
 #                          Constantes                               #
 #####################################################################
+# all_files = ['Health-Tweets/example.txt']
+
+# all_files = ['Health-Tweets/example.txt',
+# 			 'Health-Tweets/otherexample.txt']
+
 
 all_files = ['Health-Tweets/bbchealth.txt',
              'Health-Tweets/cbchealth.txt',
@@ -27,6 +34,21 @@ all_files = ['Health-Tweets/bbchealth.txt',
              'Health-Tweets/wsjhealth.txt']
 
 
+# Constantes para CountVectorizer
+min_df = 1000
+
+
+# Constantes para correr K_Means
+
+# Número de clusters.
+k = 10 
+
+# Máximo de iteraciones en cada corrida del algoritmo (si no se converge antes).
+max_iter = 5 
+
+# Número de inicializaciones distintas a realizar.
+times = 1
+
 #####################################################################
 #         Importación y preprocesamiento de los tweets              #
 #####################################################################
@@ -42,19 +64,69 @@ tweets = df['Message']
 # Utilizamos CountVectorizer para realizar la tokenización y conteo.
 # Adicionalmente se le pasa como parámetro una función preprocessor que
 # preprocesará cada tweet antes de la tokenización.
-cv = CountVectorizer(stop_words='english',
+cv = CountVectorizer(stop_words='english', min_df=min_df,
                      preprocessor=utils.preprocess_tweets)
 matrix = cv.fit_transform(tweets).toarray()
 
 
 # print(cv.vocabulary_)
 # print("-------")
-# print(matrix)
+# print(list(matrix))
 # print(cv.get_feature_names())
 # print("--------")
 # print(len(matrix))
+# names = cv.get_feature_names()
+
+data = list(matrix)
+print(len(data)) 
+print('Quitando tweets vacíos...')
+cant = 0
+for i,twe in enumerate(data):
+	if all(v == 0 for v in twe):
+		del data[i]
+		cant += 1
+print('Se termina de quitar tweets vacíos')
+print(len(data)) 
 
 
+#####################################################################
+######################### Ejercicio 1 ###############################
+#####################################################################
+
+kmeans = K_Means(k, max_iter, times)
+kmeans.train(data)
+print(kmeans.optimal[0])
+scikit_kmeans = KMeans(n_clusters=k, random_state=0, max_iter=max_iter, n_init=times).fit(data)
+print(scikit_kmeans.cluster_centers_)
+
+for centroid in scikit_kmeans.cluster_centers_:
+	for coord in centroid:
+		print(round(coord, 3))
+file = open('results.txt', 'a')
+file.write('-----------------------------------------------------')
+file.write('\n')
+file.write('ALGORITMO SCIKIT K-Means ') 
+file.write('\n')
+file.write('\n')
+file.write('Los centroides hallados scikit son: ')
+file.write('\n')
+file.write('\n')
+for centroid in scikit_kmeans.cluster_centers_:
+	for coord in centroid:
+		file.write(str(round(coord, 3)))
+		file.write(' , ')
+	file.write('\n')
+	file.write('\n')
+	file.write('*******************************')
+	file.write('\n')
+	file.write('\n')
+file.close()
+
+diff = []
+for index, centroid in enumerate(kmeans.optimal[0]):
+	diff.append(kmeans.euclidean_distance(centroid, scikit_kmeans.cluster_centers_[index]))
+
+print(diff)
 #####################################################################
 ######################### Ejercicio 3 ###############################
 #####################################################################
