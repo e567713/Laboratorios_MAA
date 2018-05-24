ad27300f0cec771da378faaf26ca40d460d490a1
Ernesto <cora.ernesto@gmail.com>
2018-05-23 23:00:23 -0300

    Ejer4

--- Laboratorio_5/main.py
+++ Laboratorio_5/main.py
@@ -1,115 +1,10 @@
 import utils
 
-#########################
-# Ejercicio del teórico #
-#########################
-
-# Data set del teórico
-S = [
-    {'Dedicacion': 'Alta', 'Dificultad': 'Alta', 'Horario': 'Nocturno',
-        'Humedad': 'Media', 'Humor Docente': 'Bueno', 'Salva': 'Yes'},
-    {'Dedicacion': 'Baja', 'Dificultad': 'Media', 'Horario': 'Matutino',
-        'Humedad': 'Alta', 'Humor Docente': 'Malo', 'Salva': 'No'},
-    {'Dedicacion': 'Media', 'Dificultad': 'Alta', 'Horario': 'Nocturno',
-        'Humedad': 'Media', 'Humor Docente': 'Malo', 'Salva': 'Yes'},
-    {'Dedicacion': 'Media', 'Dificultad': 'Alta', 'Horario': 'Matutino',
-        'Humedad': 'Alta', 'Humor Docente': 'Bueno', 'Salva': 'No'},
-]
-
-# Data set de prueba
-S2 = [{'Dedicacion': 'Alta', 'Dificultad': 'Alta', 'Horario': 'Matutino',
-       'Humedad': 'Media', 'Humor Docente': 'Bueno', 'Salva': 'Yes'},
-      {'Dedicacion': 'Baja', 'Dificultad': 'Media', 'Horario': 'Matutino',
-       'Humedad': 'Alta', 'Humor Docente': 'Malo', 'Salva': 'No'},
-      {'Dedicacion': 'Media', 'Dificultad': 'Alta', 'Horario': 'Nocturno',
-       'Humedad': 'Media', 'Humor Docente': 'Malo', 'Salva': 'Yes'},
-      {'Dedicacion': 'Media', 'Dificultad': 'Alta', 'Horario': 'Matutino',
-       'Humedad': 'Media', 'Humor Docente': 'Bueno', 'Salva': 'No'}]
-
-# Data set de prueba valores faltantes
-S3 = [
-    {'Dedicacion': 'Alta', 'Dificultad': 'Alta', 'Horario': 'Matutino',
-        'Humedad': 'Media', 'Humor Docente': 'Bueno', 'Salva': 'Yes'},
-    {'Dedicacion': 'Baja', 'Dificultad': 'Media', 'Horario': 'Matutino',
-        'Humedad': 'Alta', 'Humor Docente': 'Malo', 'Salva': 'No'},
-    {'Dedicacion': 'Media', 'Dificultad': 'Alta', 'Horario': 'Nocturno',
-        'Humedad': 'Media', 'Humor Docente': 'Malo', 'Salva': 'Yes'},
-    {'Dedicacion': 'Media', 'Dificultad': '?', 'Horario': 'Matutino',
-        'Humedad': 'Media', 'Humor Docente': 'Bueno', 'Salva': 'No'},
-]
-
-
-print('-------------------------------------------------')
-print('-------------     Ejercicio 5a     --------------')
-print('-------------------------------------------------')
-print('')
-print('Aplicacion de ID3 al ejemplo visto en el teórico')
-print('')
-
-S_entropy = utils.entropy(S, 'Salva')
-print('Entropía del conjunto: ', S_entropy)
-
-S_information_gain = utils.information_gain(S, 'Dedicacion', 'Salva', False)
-print('Information gain del atributo Dedicación: ', S_information_gain)
-S_information_gain = utils.information_gain(S, 'Humor Docente', 'Salva', False)
-print('Information gain del atributo Humor Docente: ', S_information_gain)
-S_information_gain = utils.information_gain(S, 'Horario', 'Salva', False)
-print('Information gain del atributo Horario: ', S_information_gain)
-S_information_gain = utils.information_gain(S, 'Dificultad', 'Salva', False)
-print('Information gain del atributo Dificultad: ', S_information_gain)
-S_information_gain = utils.information_gain(S, 'Humedad', 'Salva', False)
-print('Information gain del atributo Humedad: ', S_information_gain)
-
-tree = utils.ID3_algorithm(
-    S,
-    ['Dedicacion', 'Dificultad', 'Horario', 'Humedad', 'Humor Docente'],
-    'Salva',
-    True, False)
-
-utils.print_tree(tree, tree['data'], None, True, '')
-
-print()
-print()
-print('Aplicacion de ID3 a un segundo conjunto de entrenamiento')
-print()
-
-# Algoritmo aplicado al segundo conjunto de prueba
-tree2 = utils.ID3_algorithm(
-    S2,
-    ['Dedicacion', 'Dificultad', 'Horario', 'Humedad', 'Humor Docente'],
-    'Salva',
-    True, False)
-
-utils.print_tree(tree2, tree['data'], None, True, '')
-
-
-#############################################
-# Ejercicio con el data set del laboratorio #
-#############################################
-
-# Leemos data set del laboratorio
 examples = utils.read_file('Autism-Adult-Data.arff')
 data_set = examples[0]  # Datos
 metadata = examples[1]  # Metadatos
 
-print('')
-print('')
-print('')
-print('-------------------------------------------------')
-print('-------------     Ejercicio 5b     --------------')
-print('-------------------------------------------------')
-print('')
-
-# Calculamos su entropía.
-data_set_entropy = utils.entropy(data_set, 'Class/ASD')
-print('Entropía del conjunto: ', data_set_entropy)
-print()
-
-print('Aplicacion de ID3 extendido para manejar atributos numéricos')
-print('')
-
-
-# Atributos a tener en cuenta
+target_attr = 'Class/ASD'
 attributes = ['A1_Score',
               'A2_Score',
               'A3_Score',
@@ -130,94 +25,92 @@ attributes = ['A1_Score',
               'age_desc',
               'relation']
 
-# Primera solución implementada
-print('Utilizando thresholds')
-tree_2 = utils.ID3_algorithm_with_threshold(
-    data_set,
-    attributes,
-    'Class/ASD',
-    ['age'], False)
+# weight = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+weight = []
+
+categorical_atts = ['A1_Score',
+                    'A2_Score',
+                    'A3_Score',
+                    'A4_Score',
+                    'A5_Score',
+                    'A6_Score',
+                    'A7_Score',
+                    'A8_Score',
+                    'A9_Score',
+                    'A10_Score',
+                    'gender',
+                    'ethnicity',
+                    'jundice',
+                    'austim',
+                    'contry_of_res',
+                    'used_app_before',
+                    'age_desc',
+                    'relation']
+
+non_categorical_atts = ['age']
+
+n = 5
+
+categorical_atts_indexes = [0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18]
+non_categorical_atts_indexes = [10]
+
+data = utils.process_missing_values(data_set, attributes, True)
+data = utils.decode_data(data)
+
+# Se divide el conjunto de datos
+data_20, data_80 = utils.split_20_80(data)
+
+# Se arman los training y validation set
+training_data, training_target_attributes = utils.extract_target_attributes(data_80)
+
+
+
+numeric_training_set = utils.one_hot_encoding(training_data, categorical_atts, categorical_atts_indexes, non_categorical_atts, non_categorical_atts_indexes)
+validation_data, validation_target_attributes = utils.extract_target_attributes(data_20)
+# numeric_validation_set = utils.one_hot_encoding(validation_data, categorical_atts, categorical_atts_indexes, non_categorical_atts, non_categorical_atts_indexes)
+
+numeric_attributes = ['sesgo']
+numeric_attributes += list(numeric_training_set[0].keys())
+
+utils.insert_sesgo_one(numeric_training_set)
+utils.insert_target_attributes(numeric_training_set, target_attr, training_target_attributes)
+
+for i in range(len(numeric_attributes)):
+    weight += [0.1]
+
+
+print(utils.costFunction(weight, numeric_training_set, numeric_attributes, target_attr))
+weight2 = utils.descentByGradient(weight, numeric_training_set, 1, numeric_attributes, target_attr)
+# print(weight)
 print()
 
-print('Utilizando gain ratio')
-# Segunda solución implementada
-tree_3 = utils.ID3_algorithm(
-    data_set,
-    attributes,
-    'Class/ASD',
-    False, False)
-
-# Primera solución implementada valores faltantes
-tree_4 = utils.ID3_algorithm(
-    S3,
-    ['Dedicacion', 'Dificultad', 'Horario', 'Humedad', 'Humor Docente'],
-    'Salva',
-    False, False)
-# utils.print_tree(tree_4, tree_4['data'], None, True, '')
-
-
-# Segunda solución implementada valores faltantes
-tree_5 = utils.ID3_algorithm(
-    S3,
-    ['Dedicacion', 'Dificultad', 'Horario', 'Humedad', 'Humor Docente'],
-    'Salva',
-    False, True)
-# utils.print_tree(tree_5, tree_5['data'], None, True, '')
-
-print('')
-print('')
-print('')
-print('-------------------------------------------------')
-print('-------------     Ejercicio 5c     --------------')
-print('-------------------------------------------------')
-print('')
-
-# Calculamos su entropía.
-data_set_entropy = utils.entropy(data_set, 'Class/ASD')
-print('Entropía del conjunto: ', data_set_entropy)
-
-print('')
-print('Cross-Validation')
-print('')
-
-
-# Separamos el data set en dos subconjuntos
-print()
-print('Se separa el data set en dos subconjuntos')
-splitted_data = utils.split_20_80(data_set)
-
-# Verificamos la correctitud de los tamaños
-print('Tamaño del data set original: ', str(len(data_set)))
-print('Tamaño del subset de validación: ', str(len(splitted_data[0])))
-print('Tamaño del subset de entrenamiento: ', str(len(splitted_data[1])))
-
+print(utils.costFunction(weight, numeric_training_set, numeric_attributes, target_attr))
+weight = utils.descentByGradient(weight, numeric_training_set, 1, numeric_attributes, target_attr)
+# print(weight)
 print()
 
-file = open('5c_comparacion.txt', 'a')
-for i in range(2):
-  	
-    # Parte 1
-    print('Parte 1')
-    # Se realiza cross-validation de tamaño 10 sobre el 80% del conjunto original.
-    print('Se realiza 10-fold cross-validation')
-    splitted_data = utils.split_20_80(data_set)
-    cs = utils.cross_validation(
-        splitted_data[1], attributes, 'Class/ASD', 10)
-    print('Promedio de error: ', cs)
-    file.write('C-V: ' + str(cs) + ' ---- ')
-    # Parte 2
-    print('Parte 2')
+print(utils.costFunction(weight, numeric_training_set, numeric_attributes, target_attr))
+weight = utils.descentByGradient(weight, numeric_training_set, 1, numeric_attributes, target_attr)
+# print(weight)
+print()
 
-    # Se entrena con el 80%
-    tree_6 = utils.ID3_algorithm(
-        splitted_data[1],
-        attributes,
-        'Class/ASD',
-        False, False)
 
-    # Se valida con el 20%
-    v = utils.validation(
-        tree_6, splitted_data[0], 'Class/ASD')
-    print('Resultado de la validación: ', v)
-    file.write('V: ' + str(v) + '   diff: ' + str(cs-v)+ '\n')
-file.close()
+
+
+# utils.insert_target_attributes(numeric_validation_set, target_attr, validation_target_attributes)
+#
+# utils.insert_sesgo_one(numeric_validation_set, target_attr, validation_target_attributes)
+#
+# # numeric_attributes = list(numeric_training_set[0].keys())
+# numeric_attributes = list(numeric_validation_set[0].keys())
+# numeric_atts_len = len(numeric_attributes)
+#
+#
+#
+#
+#
+
+
+
+
+
